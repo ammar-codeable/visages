72bbf0186e5456cb4596a22e2137854f4c7271e5
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-01-18 00:34:34 +0530

    feat: integrate Tailwind CSS and configure Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-	return <div>Hello World</div>;
+  return (
+    <div>
+      <div className="flex text-3xl underline">Hello World</div>
+      <div className="text-xl">Hello World</div>
+    </div>
+  );
 }
 
 export default App;
