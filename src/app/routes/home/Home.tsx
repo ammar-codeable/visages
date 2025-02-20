050a1d62a62015eb01c3b42ca3d24a0613e74698
Mohammed Madni <mohammedmadni09@gmail.com>
2025-02-20 23:39:50 +0530

    feat:move pro night section to home page

--- src/app/routes/home/Home.tsx
+++ src/app/routes/home/Home.tsx
@@ -4,6 +4,8 @@ import FancyText from "@/components/FancyText";
 import VisagesHighlights from "@/components/VisagesHighlights";
 import { motion } from "motion/react";
 import VisagesLogo from "/visages-logo.png";
+import { DJNightSection } from "@/components/DJNightSection";
+import { events } from "@/constants/events";
 
 const Home = () => (
   <div className="relative min-h-screen">
@@ -42,6 +44,7 @@ const Home = () => (
       </motion.div>
 
       <VisagesHighlights />
+      <DJNightSection events={events} index={0} />
 
       <motion.div
         initial={{ opacity: 0 }}
